3593e7db7f979f99f8e8f35509db12df665dc08a
rmhancock <rosshancock91@gmail.com>
2018-10-16 15:45:09 +0100

    tests 2 and 3 passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,7 +1,14 @@
-def return_10()
+
+#test passed!
+def return_10
   return 10
 end
 
-def add(first_number, second_number)
-  return first_number.to_f() + second_number.to_f()
+#test passed!
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
 end
