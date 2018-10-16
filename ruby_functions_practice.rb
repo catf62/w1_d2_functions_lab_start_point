26a2ccde708fc7be17a8961f14c6e531340dfd86
rmhancock <rosshancock91@gmail.com>
2018-10-16 16:18:25 +0100

    test 9 worked

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -35,9 +35,23 @@ def add_string_as_number(string_1, string_2)
   return string_1_int + string_2_int
 end
 
+# case number_to_full_month_name
+# when 3
+#   return "March"
+# when 1
+#   return "January"
+# else
+#   return false
+# end
+
+
 def number_to_full_month_name(number)
   if number == 1
     return "January"
+  elsif number == 3
+    return "March"
+  elsif number == 9
+    return "September"
   else
     return "False"
   end
